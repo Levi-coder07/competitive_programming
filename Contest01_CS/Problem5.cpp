2dea28472f97ffc6fb5de5661e8d34f2e2b627a9
Levi-coder07 <82822546+Levi-coder07@users.noreply.github.com>
2022-09-28 19:46:55 -0500

    Update Problem5.cpp

--- Contest01_CS/Problem5.cpp
+++ Contest01_CS/Problem5.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
 using namespace std;
-#define N 10000
+#define N 100000
 struct almost_union_find{
-    int sets[N];
+    int *sets = new int[N];
     void init(int n){
         for(int i = 1;i<=n;i++){
             sets[i] = i;
@@ -11,6 +11,10 @@ struct almost_union_find{
     int find(int x){
         if(sets[x] == x){return x;}
         return sets[x] = find(sets[x]);
+    }
+    int find2(int x){
+        if(sets[x] == x){return x;}
+        return sets[x] = x;
     }
     bool join (int x , int y){
         x = find(x);
@@ -24,7 +28,7 @@ struct almost_union_find{
     bool move(int p,int q){
         q = find(q);
         if(sets[p]==q){
-            cout<<"HOLA";
+            
             return false;
         
         }
@@ -36,39 +40,51 @@ struct almost_union_find{
             cout<<sets[i]<<" ";
         }
     }
-    void sum(int p,int n){
+    void sum(int n,int p){
         int suma=0,count =0;
-        p = find(p);
+        
         for(int i = 1;i<=n;i++){
-            if(sets[i]==p){
+            if(sets[i]==sets[p]){
                 count++;
                 suma = suma + i;
+                
             }
            
+
         }
-        cout<<count<<" "<<suma<<endl;
+        cout<<count<<" output "<<suma<<endl;
     }
 
 };
 int main(){
+    int tipo;
     int p , q;
     int n , m;
-    int tipo;
-    while(cin>>n>>m){
+    
+    while(scanf("%d %d",&n,&m)!=EOF){
         almost_union_find elements;
         elements.init(n);
         for(int i=0;i<m;i++){
-            cin>>tipo>>p>>q;
+            scanf("%d",&tipo);
             if(tipo==1){
+                scanf("%d %d",&p,&q);
                 elements.join(p,q);
             }else if (tipo ==2){
+                scanf("%d %d",&p,&q);
                 elements.move(p,q);
             }else{
+                
+                scanf("%d",&p);
+                
                 elements.sum(n,p);
-                elements.print(n);
+                //elements.print(n);
             }
         }
 
     }
     return 0;
+    //Algorithm
+    //Goodrich
+    // Skiena
+    // Sedgewick
 }
