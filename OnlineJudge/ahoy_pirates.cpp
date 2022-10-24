d41f8bb42966e84dfbf74e241f4fc4c996fa1896
Levi Castillon <82822546+Levi-coder07@users.noreply.github.com>
2022-10-24 10:56:06 -0500

    Update ahoy_pirates.cpp

--- OnlineJudge/ahoy_pirates.cpp
+++ OnlineJudge/ahoy_pirates.cpp
@@ -1,9 +1,20 @@
 #include <iostream>
 using namespace std;
+int buildseg(int *array,int l , int r ){
+    if(l==r){
+        return;
+    }
+    int mid = l+r;
+    buildseg(array,l,mid);
+    buildseg(array,mid+1,r);
+}
 int main(){
-    int Test;
+    int Test,n;
     cin>>Test;
     for(int i = 0;i<Test;i++){
-        
+        cin>>n;
+        while(n--){
+
+        }
     }
 }
