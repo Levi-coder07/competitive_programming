24318f4ffacb802d7e1d1d6c19e23df58b02658c
Levi <yulevja24@hotmail.com>
2022-10-29 09:35:42 -0500

    Marbles

--- OnlineJudge/marbles.cpp
+++ OnlineJudge/marbles.cpp
@@ -1,30 +1,59 @@
 #include <iostream>
 #include <vector>
 using namespace std;
-int Smallest(){}
+
 struct vertex{
-    int marbles; 
-    bool child;
+    int marbles;
+    int moves;
+    bool ischild;
     vector<vertex*> son;
 };
+int Smallest(vertex* actual){
+    if(actual->son.empty()){
+        actual->moves = actual->marbles - 1;
+        return abs(actual->moves);
+    }
+    int count = 0;
+
+    for(int i = 0;i<actual->son.size();i++){
+        count += Smallest(actual->son[i]);
+        actual->moves += actual->son[i]->moves; 
+        
+    }
+    if(actual->ischild==true){
+        actual->moves += actual->marbles - 1;
+        count += abs(actual->moves);
+    }
+    return count;
+}
 int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
-    int cases = 3;
+    int cases = 10;
+    int result;
     int n,v,m,d,children,k;
     while(cases--){
         cin>>n;
         if(n==0){break;}
-        vertex array_ver[n];
+        vertex *array_ver = new vertex[n];
         for(int i = 0 ; i<n;i++){
             cin>>v>>m>>d;
+            --v;
             array_ver[v].marbles = m;
-            cout<<"Marbles from v : "<<array_ver[v].marbles<<endl;
+            array_ver[v].me = v;
             for(int j = 0 ; j<d;j++){
                 cin>>k;
+                --k;
                 array_ver[v].son.push_back(&array_ver[k]);
+                array_ver[k].ischild = true;
             }
         }
-        Smallest();
+        for(int i = 0 ; i<n;i++){
+            if(array_ver[i].ischild!=true){
+                result = Smallest(&array_ver[i]);
+                break;
+            }
+        }
+        cout<<result<<endl;
     }
 }
