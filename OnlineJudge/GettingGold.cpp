dd3575ac693c3a3f334ee7c60b62cba85067289c
Levi <yulevja24@hotmail.com>
2022-11-05 01:14:19 -0500

    Graph Problem

--- OnlineJudge/GettingGold.cpp
+++ OnlineJudge/GettingGold.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 char map[50][50];
-bool visited[51][51];
+bool visited[50][50];
 int directions[4][2] = {
 		        { 0,1 },
 		        { -1,0 },
@@ -22,7 +22,8 @@ void dfs(int x , int y,int filas , int columnas){
     
     for(int i = 0;i<4;i++){
         int xx = x + directions[i][0] ; int yy = y + directions[i][1];
-        if(xx>=0 && yy>=0 && xx<filas && yy <columnas&&map[xx][yy]=='T'||visited[xx][yy]){
+        if(xx>=0 && yy>=0 && xx<filas && yy <columnas&&map[xx][yy]=='T'){
+           
             return;
         }
     }
@@ -36,11 +37,9 @@ void dfs(int x , int y,int filas , int columnas){
     
 }
 int main(){
-    
-    int cases;
     int filas,columnas;
     int x , y ;
-    while(cin>>columnas>>filas){
+    cin>>columnas>>filas;
         for(int i = 0;i<filas;i++){
             for (int j = 0; j < columnas; j++)
             {
@@ -55,13 +54,5 @@ int main(){
         gold = 0;
         dfs(x,y,filas,columnas);
         cout<<gold<<endl;
-        for(int i = 0;i<filas;i++){
-            for (int j = 0; j < columnas; j++)
-            {
-               cout<<visited[i][j];
-            }
-            cout<<endl;
-        }
         
-    }
 }
