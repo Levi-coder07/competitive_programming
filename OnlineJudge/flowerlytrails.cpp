b599f3af13e31dd682b3795fc87aa0aff8cecdfe
Levi <yulevja24@hotmail>
2022-11-06 00:31:03 -0500

    Solving

--- OnlineJudge/flowerlytrails.cpp
+++ OnlineJudge/flowerlytrails.cpp
@@ -1,5 +1,29 @@
 #include <iostream>
+#include <vector>
+#include <tuple>
 using namespace std;
+
+struct graph{
+    vector<tuple<int,int,int>> aristas;
+    int points;
+};
 int main(){
-    int cases;
+    graph G;
+    int p , t;
+    int p1 , p2 , l;
+    while(cin>>p>>t){
+        G.points = p;
+        for(int i = 0;i<t;i++){
+            cin>>p1>>p2>>l;
+            cout<<"HOLA";
+            G.aristas.push_back(make_tuple(p1,p2,l));
+           
+        }
+        for(int i = 0;i<t;i++){
+            
+            cout<<get<0>(G.aristas[i]);
+        }
+
+
+    }
 }
